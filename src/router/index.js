014fb26edabd57a32690b880fc2477590d797503
Chris Davis <chris@tinyships.io>
2021-04-25 09:44:52 -0400

    remove unused routes

--- src/router/index.js
+++ src/router/index.js
@@ -17,30 +17,6 @@ const routes = [
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
   {
-    path: "/brazil",
-    name: "Brazil",
-    component: () =>
-      import(/* webpackChunkName: "brazil" */ "../views/Brazil.vue"),
-  },
-  {
-    path: "/hawaii",
-    name: "Hawaii",
-    component: () =>
-      import(/* webpackChunkName: "hawaii" */ "../views/Hawaii.vue"),
-  },
-  {
-    path: "/jamaica",
-    name: "Jamaica",
-    component: () =>
-      import(/* webpackChunkName: "jamaica" */ "../views/Jamaica.vue"),
-  },
-  {
-    path: "/panama",
-    name: "Panama",
-    component: () =>
-      import(/* webpackChunkName: "panama" */ "../views/Panama.vue"),
-  },
-  {
     path: "/details/:id",
     name: "Destination Details",
     component: () =>
